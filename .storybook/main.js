1e6b3e6a4bc108eec91f0fcd2347ee95ba9c8024
AmagiDDmxh <amagiddmxh@qq.com>
2021-09-17 02:33:18 +0800

    Update storybook config to support tailwind

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,31 @@
+const path = require('path')
+
+const resolve = (...paths) => path.resolve(__dirname, ...paths)
+
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../components/**/*.stories.@(js|jsx|ts|tsx)',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        {
+          loader: 'postcss-loader',
+          options: {
+            ident: 'postcss',
+            plugins: [
+              require('tailwindcss/nesting'),
+              require('tailwindcss'),
+              require('autoprefixer'),
+            ],
+          },
+        },
+      ],
+      include: [resolve('../src'), resolve('../components')],
+    })
+    return config
+  },
 }
